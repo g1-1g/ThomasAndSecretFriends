adb8d5152a71a431838c98405cd753c04101ae40
g1-1g <ajw0904@naver.com>
2023-12-02 00:56:29 +0900

    12/2
    할일 완

---
+++ gameTeamWalk/Assets/Pick.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Pick : MonoBehaviour
+{
+    int count = 0;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject.CompareTag("Key"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;  
+        }
+    }
+}
